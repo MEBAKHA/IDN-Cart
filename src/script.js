5ea3ae4bd999208ab286764baa6bf623d7faba77
MEBAKHA <koboycilok@gmail.com>
2026-01-23 10:10:04 +0700

    categories & ad section

--- src/script.js
+++
@@ -1,4 +0,0 @@
-const button = document.getElementById('Alert-button');
-button.addEventListener('click', function(){
-    console.log("you can Buy the something in here. dont forget my friend this website is top 1 in the world 😊");
-});
