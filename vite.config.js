a74900b1512aac20e7c77a861d8980e962a74de8
MEBAKHA <koboycilok@gmail.com>
2026-02-05 10:44:11 +0700

    hahay

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,25 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+
+  base:"./",
+  build:{
+    rollupOptions:{
+      input:{
+        main: 'index.html',
+        account: 'account.html',
+        checkout: 'checkout.html',
+        login: 'login.html',
+        register: 'register.html',
+        shop: 'shop.html',
+        checkout: 'checkout.html',
+        whislist: 'whistlist.html',
+        account: 'account.html',
+        view: 'view.html'
+
+      }
+    }
+  },
   plugins: [
     tailwindcss(),
   ],
